ee9b43d9cf038bf8325424d929840e56f5665e00
Raul <rey_brujo@hotmail.com>
2021-05-15 13:49:00 +0900

    upload

---
+++ Assets/Scripts/MapGridObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Represents a single Grid position
+ * */
+public class MapGridObject {
+
+    public enum Type {
+        Empty,
+        Mine,
+        MineNum_1,
+        MineNum_2,
+        MineNum_3,
+        MineNum_4,
+        MineNum_5,
+        MineNum_6,
+        MineNum_7,
+        MineNum_8,
+    }
+
+    private Grid<MapGridObject> grid;
+    private int x;
+    private int y;
+    private Type type;
+    private bool isRevealed;
+    private bool isFlagged;
+
+    public MapGridObject(Grid<MapGridObject> grid, int x, int y) {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        type = Type.Empty;
+        isRevealed = false;
+    }
+
+    public int GetX() => x;
+    public int GetY() => y;
+
+    public Type GetGridType() {
+        return type;
+    }
+
+    public void SetGridType(Type type) {
+        this.type = type;
+    }
+
+    public void SetFlagged() {
+        isFlagged = true;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public bool IsFlagged() {
+        return isFlagged;
+    }
+
+    public void Reveal() {
+        isRevealed = true;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public bool IsRevealed() {
+        return isRevealed;
+    }
+
+    public override string ToString() {
+        return type.ToString();
+    }
+
+}
---
+++ Assets/Scripts/UI_GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CodeMonkey.Utils;
+
+public class UI_GameOver : MonoBehaviour {
+
+    public static UI_GameOver Instance { get; private set; }
+
+    private void Awake() {
+        Instance = this;
+        transform.Find("retryBtn").GetComponent<Button_UI>().ClickFunc = () => SceneManager.LoadScene(0);
+        Hide();
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    public void Show() {
+        gameObject.SetActive(true);
+        transform.SetAsLastSibling();
+    }
+
+}
